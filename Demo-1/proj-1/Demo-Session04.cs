a268e18f7f7f354af80e79c287fea33ab150087a
fatema123elfiky <fatmaelfeky922@gmail.com>
2026-06-25 19:18:11 +0300

    Add project files.

---
+++ Demo-1/proj-1/Demo-Session04.cs
@@ -0,0 +1,53 @@
+﻿// not mandatory to use
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj_1
+{
+    internal class Demo_Session04
+    {
+
+        static void Main(string[] args)
+        {
+            #region Try Parse example
+
+                Console.Write("Enter ur age: ");
+                bool flag = int.TryParse(Console.ReadLine(), out int age);
+                Console.WriteLine(flag);
+                Console.WriteLine(age);
+
+            #endregion
+
+            #region ToString example
+            double X = 10000000;
+            DateTime date = DateTime.Now;
+
+            Console.WriteLine(X.ToString("N1"));
+            Console.WriteLine(X.ToString("0.00"));
+            Console.WriteLine(X.ToString("C"));
+            Console.WriteLine(date.ToString("t"));// short time
+            Console.WriteLine(date.ToString("T"));// long time
+            Console.WriteLine(date.ToString("d"));// short date
+            Console.WriteLine(date.ToString("D"));// long date
+            Console.WriteLine(date.ToString("dd-MM-yyyy"));// long date
+
+
+            Console.WriteLine(X.ToString("C"));
+            // same to
+            Console.WriteLine($"{X:c}");
+
+            // previous part is for builtin tostring and premade as override by c#
+            // now for classes made by me , now we should override the tostring method 
+
+
+
+
+
+
+            #endregion
+        }
+    }
+}
---
+++ Demo-1/proj-1/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace proj_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Try catch
+
+            /*try
+            {
+                string s = null;
+                s.ToUpper();
+
+                int X = 0, Y = 10;
+                int Z = Y/X;
+            }
+            catch(NullReferenceException ex ) // problem of  repeation
+            {
+                Console.WriteLine( ex.Message );
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }*/
+
+
+            try
+            {
+                // db connection
+                string s = null;
+                s.ToUpper();
+
+                int X = 0, Y = 10;
+                int Z = Y / X;
+            }
+            catch (Exception e) { 
+                Console.WriteLine(e.Message);
+            }
+
+            finally
+            {
+                // close db connection
+                // that region will be implemented after the finish of try if no error
+                // or after catch if there is an error
+                // as catch stops the rest of program if there is an error but finally will continue the rest of the program
+            }
+
+            /*
+             Actually, the code does continue after catch unless:
+            the program crashes completely
+            you use throw
+            you use return
+            environment terminates execution
+             */
+
+
+
+
+            /*
+             If there were no catch, the exception 
+            would usually terminate the program after finally.
+            and if there is catch the code continues normally
+             */
+
+
+            try
+            {
+                string s = null;
+                s.ToUpper();
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+
+
+
+
+            Console.WriteLine("hello");
+
+            Console.WriteLine("Welcome batata");
+            int w = 1, u = 2, g;
+            Console.WriteLine(u / w);
+
+            #endregion
+
+        }
+    }
+}
